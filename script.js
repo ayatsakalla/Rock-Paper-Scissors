a2483cbc752b80ad409ff80d8bef2ca5b2fb1b27
ayatsakalla <ayat.sakalla@outlook.com>
2024-02-26 13:49:11 -0500

    added username selection in js and added some more rows with info

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+// collecting player username start
+let userName = prompt('Enter Username:');
+let namez = `${userName}`;
+let playerName = document.querySelector('#name');
+playerName.innerHTML = `${namez}`;
+// collecting player username end
+
+
