f07cf91ffce88438ea90003ea0e0c44270cfedbb
ayatsakalla <ayat.sakalla@outlook.com>
2024-02-28 08:25:15 -0500

    playRound for both computer and player + imgs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerImg = document.getElementById('playerChoiceImg');
+let compImg = document.getElementById('compChoiceImg');
+
 
 // collecting player username start
 let userName = prompt('Enter Username:');
@@ -6,4 +9,23 @@ let playerName = document.querySelector('#name');
 playerName.innerHTML = `${namez}`;
 // collecting player username end
 
+function playRound(userChoice) {
+    //get computer choice
+    let compChoice
+    let randomNum = Math.floor(Math.random()*3);
+    //turn randomNum into string value
+    if (randomNum === 0) {
+        compChoice = 'rock';
+    } else if (randomNum===1) {
+        compChoice = 'paper';
+    } else {
+        compChoice = 'scissors';
+    }
 
+    //display choices 
+
+    playerImg.src = `${userChoice}.jpg`
+    compImg.src = `${compChoice}.jpg`
+
+
+}
